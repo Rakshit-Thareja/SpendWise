58b708d28064081622804ec71d185883ed13df92
Rakshit-Thareja <jaythareja123@gmail.com>
2026-09-04 14:15:29 +0530

    Fix footer import casing

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import { useState } from 'react'
 
 function DashboardLayout({ children }) {
